855dadd230f76eb39fce148c72b1dc4434c5e4ec
jason71319jason <jason71319jason@gmail.com>
2020-05-30 00:58:58 +0800

    Add server function and related testcases

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,13 +1,17 @@
 public class Server {
 
+    public static final int DEFAULT_PORT = 12345;
+    private Status status;
+    private int port;
     /**
-     *
+     * Server constructor
      */
     public Server() {
 
     }
 
     /**
+     * Server constructor
      * @param port binding port
      */
     public Server(int port) {
@@ -21,4 +25,26 @@ public class Server {
 
     }
 
+    /**
+     * Shutdown server
+     */
+    public void shutdown() {
+
+    }
+
+    /**
+     * Get server's status
+     * @return server's status
+     */
+    public Status getStatus() {
+        return null;
+    }
+
+    /**
+     * Get server's binding port
+     * @return server's binding port
+     */
+    public int getPort() {
+        return -1;
+    }
 }
--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -1,10 +1,57 @@
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 public class ServerTest {
 
     @Test
-    public void run() {
+    public void server_defaultPort() {
+        Server server = new Server();
+        Assert.assertEquals(server.getPort(), Server.DEFAULT_PORT);
+    }
+
+    @Test
+    public void server_givenNonOccupiedPort() {
+        int givenPort = 54321;
+        Server server = new Server(givenPort);
+        Assert.assertEquals(server.getPort(), givenPort);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void server_givenInvalidPort() {
+        int invalidPort = -1;
+        Server server = new Server(invalidPort);
+    }
+
+    @Test (expected = IOException.class)
+    public void run_OccupiedPort() {
+        Server server = new Server();
+        server.run();
+        Server error_server = new Server();
+        error_server.run();
+    }
+
+    @Test
+    public void run_defaultPort() {
+        Server server = new Server();
+        server.run();
+        Assert.assertEquals(server.getStatus(), Status.ACTIVE);
+    }
+
+    @Test
+    public void run_givenPort() {
+        Server server = new Server();
+        server.run();
+        Assert.assertEquals(server.getStatus(), Status.ACTIVE);
+    }
+
+    @Test
+    public void shutdown_success() {
+        Server server = new Server();
+        server.shutdown();
+        Assert.assertEquals(server.getStatus(), Status.INACTIVE);
     }
 }
